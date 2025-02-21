de30ab23bc92122ea327555319c3c3f83e6869f4
Hope-WFA <hope@webfuelagency.com>
2025-02-21 10:54:44 +0800

    first commit

---
+++ lib/hello.dart
@@ -0,0 +1,13 @@
+library hello;
+
+/// A Calculator.
+class Calculator {
+
+  int _value = 0;
+
+  /// Returns [value] plus 1.
+  int addOne(int value) => _value + 1;
+
+
+  int get value => _value;
+}
